1ac35aed2883d146ce225ff2207e628d4ea6b6a2
oppressionslayer <37212196+oppressionslayer@users.noreply.github.com>
2020-05-31 06:25:40 -0600

    Update larsprime.py

--- larsprime.py
+++ larsprime.py
@@ -353,3 +353,238 @@ def powers_nonfactorization_quantum_leap(hm):
 """
 def get_last_modulus_powers_of_two(hm):
    return gcd(hm, 1<<hm.bit_length())
+
+
+""" Fuzzy factor was designed for finding psuedoprimes near powers of 2 numbers that can be reduced faster than a 
+   straight factorization using many differerent algotihims. For example, Try: fuzzy_factor(2**1200-1, True) and 
+   you get an almost instantaneous result with only 3 numbers that are False (psuedoprime). You can plug these numbers
+   into PARI or ECM and they can factor the numbers faster than you can by using PARI and ECM to reduce just the
+   2**1200-1 itself.  PARI can't seem to factor 2**1200-1 after an hour run, so you can factor the number using fuzzy
+   factor then further reduce the psuedoprimes which it indicates and get the complete factorization for 2**1200-1.
+    
+   This module factors as best as it can. If a number is too slow for modular reduction, it gives the best possible
+   psuedoprime as an answer. This helps get an idea for the factors of very large numbers. Numbers that return
+   themselves are composed of very large primes that are to slow for modular reduction and until a faster modular
+   reduction technique is found by me or others, Use https://www.alpertron.com.ar/ECM.HTM to further reduce the
+   number.
+   What is *AMAZING* about fuzzy_factor. https://www.alpertron.com.ar/ECM.HTM cannot reduce 2**1000-1, but using 
+   fuzzy_factor, you can take the psuedoprime components, plug them into ECM, and fully reduce 2*1000-1 in a few
+   minutes, as fuzzy_factor reduces the number into psuedoprimes that can be reduced further by ECM instead of a
+   straight factorization of 2**1000-1. Use fuzzy_factor(2**1000-1, True) to get results back with whether the
+   number is prime (True) or psuedoprime (False). You can then plug the False numbers into ECM to reduce the number
+   into all of its factors. The default is False so you can use with build_prime_number() to build the original number 
+   from the fuzzy factors. You can also try this with 2**500-1 and other numbers to see it's usefulness in combination
+   with other factorization engines.
+
+   Here is an example output, try this with your favorite factorization engine in comparison to this usage:
+
+   In [5]: build_prime_number(fuzzy_factorp2(2**1200-1))                                                                                                                  
+   Out[5]:
+17218479456385750618067377696052635483579924745448689921733236816400740691241745619397484537236046173286370919031961587788584927290816661024991609882728717344659503471655990880884679896520055123906467064419056526231345685268240569209892573766037966584735183775739433978714578587782701380797240772477647874555986712746271362892227516205318914435913511141036261375
+
+   fuzzy_factorp2(2**1200-1,True) 
+
+   Out[2606]: 
+[(3, True),
+ (3, True),
+ (5, True),
+ (7, True),
+ (13, True),
+ (5, True),
+ (5, True),
+ (31, True),
+ (11, True),
+ (41, True),
+ (17, True),
+ (257, True),
+ (151, True),
+ (251, True),
+ (1601, True),
+ (101, True),
+ (401, True),
+ (331, True),
+ (61, True),
+ (97, True),
+ (241, True),
+ (4051, True),
+ (1201, True),
+ (8101, True),
+ (673, True),
+ (1801, True),
+ (601, True),
+ (61681, True),
+ (4801, True),
+ (1321, True),
+ (340801, True),
+ (63901, True),
+ (25601, True),
+ (55201, True),
+ (268501, True),
+ (100801, True),
+ (10567201, True),
+ (4278255361, True),
+ (4562284561, True),
+ (1133836730401, True),
+ (8846144025137201, False),
+ (18518800563924107521, False),
+ (13334701, True),
+ (1182468601, True),
+ (35657512630090883498190108804189845169601, False),
+ (1461503031127477825099979369543473122548042956801, True),
+ (8059720126266442627050052102446681278605043839701907629253987599434464819580116421853601,
+  True)]
+
+  You can than then use ECM or PARI to further reduce the False (psuedoprimes) to their factors faster than 
+  current straight PARI or ECM factorizations of 2**1200-1.
+
+"""
+
+def fuzzy_factorp2(hm, returnwithpsuedoprimeresults=False):
+   b = num = hm
+   vv = []
+   lprime = False
+   larstest = [-2, -1, 0, 1, 2]
+   while larsprimetest(num) == False and num != 1:
+     for x in larstest:
+       b = get_factor_lars_prime(num, x)[0]
+       if larsprimetest(b) == True:
+         lprime = True
+         break
+     if returnwithpsuedoprimeresults == False:
+        vv.append(b)
+     elif returnwithpsuedoprimeresults == True:
+        vv.append((b, larsprimetest(b)))
+     num = num // b
+   if num != 1:
+     if lprime == True:
+       if returnwithpsuedoprimeresults == False:
+          vv.append(num)
+       elif returnwithpsuedoprimeresults == True:
+          vv.append((num, larsprimetest(num))) 
+     elif lprime == False:
+       if returnwithpsuedoprimeresults == False:
+          vv.append(b)
+       elif returnwithpsuedoprimeresults == True:
+          vv.append((b, larsprimetest(b))) 
+   #print(vv)
+   return vv
+
+
+""" This fuzzy factor factorizes more than the fuzzy_factor. In doing so it's slower but time constrained so it 
+    shouldn't take more than 5-10 minutes on any factor given and faster than that on smaller numbers. It has two
+    forms of use, one is fuzzy_factor_time_constrained(100983434321342423242314234234242432) and the other is
+    fuzzy_factor_time_constrained(100983434321342423242314234234242432, True) so you can see which answers are 
+    psuedoprime
+
+   In [614]: fuzzy_factor_time_constrained(100983434321342423242314234234242432, True)                                                                                    
+   Out[614]: 
+   [(2, True),
+   (2, True),
+   (2, True),
+   (2, True),
+   (2, True),
+   (2, True),
+   (2, True),
+   (31, True),
+   (7, False),
+   (31, True),
+   (74363, True),
+   (80111, True),
+   (19686553169943826729, False)]
+
+   For powers of two -1 use fuzzy_factorp2. It is much, much faster.
+
+"""
+
+def fuzzy_factor_time_constrained(hm, returnwithpsuedoprimeresults=False):
+   num = hm
+   vv = []
+   if larsprimetest(hm):
+      return f"{hm} is already prime"
+   larstest = [-2, -1, 0, 1, 2]
+   while larsprimetest(num) == False and num != 1:
+     for x in larstest:
+       b = get_factor_lars_prime2(num, x)[0]
+       #print(b)
+       if larsprimetest(b) == True:
+         break
+     if returnwithpsuedoprimeresults == False:
+        vv.append(b)
+     elif returnwithpsuedoprimeresults == True:
+        vv.append((b, larsprimetest(b)))
+     num = num // b
+     if isprime(num):
+        break
+   if larsprimetest(num):
+     if returnwithpsuedoprimeresults == False:
+        vv.append(num)
+     elif returnwithpsuedoprimeresults == True:
+        vv.append((num, larsprimetest(b))) 
+   elif num != 1:
+     if returnwithpsuedoprimeresults == False:
+        vv.append(b)
+     elif returnwithpsuedoprimeresults == True:
+        vv.append((b, larsprimetest(b))) 
+   return vv
+
+""" These modules are utilized by fuzzy factors """
+
+def find_prime_evens2(hm, offset=-1):
+   y = 3
+   prevtemp = 3
+   newtemp = 0
+   orighm = hm
+   count = 0
+   while True:
+     if count > 3000:
+       return lars_find_prime(orighm, offset)
+     count += 1
+     if y.bit_length() > hm.bit_length() -1:
+       hm = hm**2
+     j = powers(hm, y)
+     temp = j
+     if j != 1 and j != 0:
+       temp = j
+       temp = hm % temp   
+       if temp == 0:
+          prevtemp = temp
+          break   
+       while temp != 1 and temp != 0:
+          prevtemp = temp
+          temp = hm % temp
+     if temp == 0:
+       #print("h: break")
+       break
+     y = Xploder(y) -offset
+     if count == 1:
+        newtemp = hm
+   return hm, j, y.bit_length(), y, temp, prevtemp, count
+
+""" These modules are utilized by fuzzy factors """
+
+def get_factor_lars_prime2(hm, offset=-1):
+     num = hm
+     vv = []
+     a = find_prime_evens2(num, offset)
+     if a[5] == 0:
+       vv.append(a[1])
+     elif a[5] != 0 and a[3] == hm:
+       vv.append(a[3])
+     elif a[5] == 2:
+       if num % 2 == 0:
+         vv.append(a[5])
+       else:
+         vv.append(a[3])
+     elif a[5] == 3:
+       if num % 2 == 0:
+          vv.append(2)
+       elif num % 3 == 0:
+          vv.append(3)
+       elif num % 5 == 0:
+          vv.append(5)
+       elif num % a[3] == 0:
+          vv.append(a[3])
+     else:
+       vv.append(a[5])
+     num = num // vv[-1]
+     return vv
