1efefe78e80c09093aded1271e99e33506bed31b
oppressionslayer <37212196+oppressionslayer@users.noreply.github.com>
2020-06-05 22:56:42 -0600

    Update larsprime.py

--- larsprime.py
+++ larsprime.py
@@ -669,13 +669,15 @@ def larsrandomprimemaker(smallend, largeend, withstats=False):
    elif withstats == False:
       return num//2
 
-""" This is probabalistic due to it being unproven that all numbers greater than 2**50 pass the fermat(2, num-1, num) test when
-    lars_last_modulus_powers_of_two(num+num) == 2. It is extremely quick and using a stress test i haven't found any numbers that
-    haven't passed this test, but with that said, there is no theory proving it's accuracy, so use it knowing that. This test is only
-    to be used for primes larger than 2**50. This is really for educational purposes only, but you may find it to be practical if 
-    you looking for a quick prime test and even with weeks of cpu time of stress testing i haven't seen any errors in it's answers.
+""" START PROBABALISTIC USE FOR EDUCATIONAL PURPOSES ONLY """            
+
+""" The following is probabalistic due to the ability to use mersenne numbers to generate primes that do not pass the test, but
+    without using those, it has an uncanny accurary rate.
     I include it for educational purposes not practicle purposes, but i think you'll be suprised by it's ability to pass any isprime
-    test.
+    test, that dont use mersenne numbers to generate the prime. So don't use it for production because you can use mersenne numbers to
+    generate primes that don't pass the test. Either way i include it here for educational purposes and it's uncanny ability to
+    generate primes that were not generated from a mersenne number. I can include more information on that here if anyone
+    is interested
 """ 
 
 def fast_probabilistic_isprime(hm):
@@ -688,7 +690,9 @@ def fast_probabilistic_isprime(hm):
     else:
        return False
 
-""" This uses the probabalistic fast isprime function to find the next prime
+""" This uses the probabalistic fast isprime function to find the next prime. Do not use for production i include
+    if here because it is very good at making primes, but you can use mersenne numbers to generate primes that don't pass
+    the test. Either way i include it here for educational purposes only. 
 """
 
 def fast_probabilistic_next_prime(hm):
@@ -702,7 +706,9 @@ def fast_probabilistic_next_prime(hm):
    return hm
 
 """ This creates a random number and then finds the next prime using he probabalistic fast isprime function. It's usage is:
-    create_probabilistic_prime(1500). You must use a number greater than 50.
+    create_probabilistic_prime(1500). You must use a number greater than 50.  Do not use for production i include
+    if here because it is very good at making primes, but you can use mersenne numbers to generate primes that don't pass
+    the test. Either way i include it here for educational purposes only. 
 """
 
 def create_probabilistic_prime(hm):
@@ -712,9 +718,9 @@ def create_probabilistic_prime(hm):
    return fast_probabilistic_next_prime(num)
 
 
-""" This is a probabalistic primemaker based on using fast_probabalistic_isprime(). Use at your own risk, it is based on the reasoning
-    that the chance of error is very small. You can always use larsprimetest or sympy's isprime to verify the results. I have stressed
-    test this with weeks of cpu time on a 6 core AMD and have seen no erronous results on numbers larger than 2**50. 
+""" This is a probabalistic primemaker based on using fast_probabalistic_isprime(). Do not use for production i include
+    if here because it is very good at making primes, but you can use mersenne numbers to generate primes that don't pass
+    the test. Either way i include it here for educational purposes only. 
 """
 
 def larsprobabilisticprimemaker(smallend, largeend, withstats=False):
@@ -737,4 +743,61 @@ def larsprobabilisticprimemaker(smallend, largeend, withstats=False):
    elif withstats == False:
       return num//2
 
+""" Prime Sieve Maker found on the internet """"
 
+def primes_sieve2(limit):
+    a = [True] * limit
+    a[0] = a[1] = False
+
+    for (i, isprime) in enumerate(a):
+        if isprime:
+            yield i
+            for n in range(i*i, limit, i):
+                a[n] = False
+
+""" END PROBABALISTIC FOR EDUCATIONAL PURPOSES ONLY """            
+            
+
+""" mersenne_prime_maker() Here is it's description:
+    Use any number here but use the sieves numbers here to see that only prime numbers can make prime numbers
+    via pow(xx, 2**x-1, 2**x). Where xx is a prime number and x is the iteration of the loop. For example:
+
+    In [8128]: mersenne_prime_maker(6)                                                                                                                                     
+    pow(53, 2**6-1, 2**6) = 29 and is Prime, 53 is Prime?: False, 2**6-1 is Prime?: True
+    pow(43, 2**6-1, 2**6) = 3 and is Prime, 43 is Prime?: False, 2**6-1 is Prime?: True
+    pow(31, 2**6-1, 2**6) = 31 and is Prime, 31 is Prime?: False, 2**6-1 is Prime?: True
+    pow(29, 2**6-1, 2**6) = 53 and is Prime, 29 is Prime?: False, 2**6-1 is Prime?: True
+    pow(13, 2**6-1, 2**6) = 5 and is Prime, 13 is Prime?: False, 2**6-1 is Prime?: True
+    pow(5, 2**6-1, 2**6) = 13 and is Prime, 5 is Prime?: False, 2**6-1 is Prime?: True
+    pow(3, 2**6-1, 2**6) = 43 and is Prime, 3 is Prime?: False, 2**6-1 is Prime?: True
+
+    In [8129]: mersenne_prime_maker(7)                                                                                                                                      
+    7: 127 already Prime
+    pow(113, 2**7-1, 2**7) = 17 and is Prime, 113 is Prime?: True, 2**7-1 is Prime?: True
+    pow(109, 2**7-1, 2**7) = 101 and is Prime, 109 is Prime?: True, 2**7-1 is Prime?: True
+    pow(107, 2**7-1, 2**7) = 67 and is Prime, 107 is Prime?: True, 2**7-1 is Prime?: True
+    pow(101, 2**7-1, 2**7) = 109 and is Prime, 101 is Prime?: True, 2**7-1 is Prime?: True
+    pow(79, 2**7-1, 2**7) = 47 and is Prime, 79 is Prime?: True, 2**7-1 is Prime?: True
+    pow(67, 2**7-1, 2**7) = 107 and is Prime, 67 is Prime?: True, 2**7-1 is Prime?: True
+    pow(53, 2**7-1, 2**7) = 29 and is Prime, 53 is Prime?: True, 2**7-1 is Prime?: True
+    pow(47, 2**7-1, 2**7) = 79 and is Prime, 47 is Prime?: True, 2**7-1 is Prime?: True
+    pow(43, 2**7-1, 2**7) = 3 and is Prime, 43 is Prime?: True, 2**7-1 is Prime?: True
+    pow(29, 2**7-1, 2**7) = 53 and is Prime, 29 is Prime?: True, 2**7-1 is Prime?: True
+    pow(17, 2**7-1, 2**7) = 113 and is Prime, 17 is Prime?: True, 2**7-1 is Prime?: True
+    pow(3, 2**7-1, 2**7) = 43 and is Prime, 3 is Prime?: True, 2**7-1 is Prime?: True
+
+
+    Use https://www.mersenne.org/primes/  to find which primes are mersenne primes to test. Notice that in both
+    cases the numbers generated show up as the first number in the pow() statement that generate a true prime,
+    even if false in the case of non mersenne numbers. I thought this was interesting so included it in this library.
+
+"""
+
+
+def mersenne_prime_maker(x):
+   primereducer = list(reversed(list(primes_sieve2(2**x-1))))
+   if larsprimetest(2**x-1) == True:
+      print(f"{x}: {2**x-1} already Prime")
+   for xx in primereducer:
+      if larsprimetest(pow(xx, 2**x-1, 2**x)) == True:
+        print(f"pow({xx}, 2**{x}-1, 2**{x}) = {pow(xx, 2**x-1, 2**x)} and is Prime, {xx} is Prime?: {larsprimetest(x)}, 2**{x}-1 is Prime?: {larsprimetest(pow(xx, 2**x-1, 2**x))}")
